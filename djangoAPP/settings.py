204cb896b928c74b4e4f1585728d0ed515f8852e
watanabe-atrasc <watanabesa@atrasc.co.jp>
2019-03-25 13:59:52 +0900

    commit

--- djangoAPP/settings.py
+++ djangoAPP/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'djangoAPP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'watanabe@postgresqlap-postgresqldbserver',
+        'USER': 'watanabe',
+        'PASSWORD': 'Sa1234567',
+        'HOST': 'postgresqlap-postgresqldbserver.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
 
