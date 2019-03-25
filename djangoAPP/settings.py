a5924d9fe9487d7d3d98c2746f04f0062a695745
watanabe-atrasc <watanabesa@atrasc.co.jp>
2019-03-25 09:47:41 +0900

    commit

--- djangoAPP/settings.py
+++ djangoAPP/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'djangoAPP.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite4'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
