0fb1c968216870d26408eefc4e3ce581c478be0c
watanabe-atrasc <watanabesa@atrasc.co.jp>
2019-03-26 16:09:57 +0900

    commit

--- djangoAPP/settings.py
+++ djangoAPP/settings.py
@@ -80,11 +80,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        
          'OPTIONS': {
-            'connection_timeout': 10, 
+            'timeout': 10, 
         },
-        
         
     }
 }
