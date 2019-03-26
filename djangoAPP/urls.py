6b47f3c578f454a9e8191a0c9652ad7898010c86
watanabe-atrasc <watanabesa@atrasc.co.jp>
2019-03-26 11:57:56 +0900

    commit

--- djangoAPP/urls.py
+++ djangoAPP/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('API/', include('API.urls')),
+    path('sqlite/admin/', admin.site.urls),
+    path('sqlite/API/', include('API.urls')),
+    
+    #path('admin/', admin.site.urls),
+    #path('API/', include('API.urls')),
+    
 ]
